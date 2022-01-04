b79a348dd972498986c629aac1ac8b8dc577a5e7
Rameez-28 <rameezmalik93@gmail.com>
2022-01-04 17:11:23 +0500

    second

---
+++ 1/index_files/index.js.download
@@ -0,0 +1,3 @@
+console.log('Muhammad Rameez Malik');
+var myAge = 28;
+console.log(myAge);
---
+++ 2/index.js
@@ -0,0 +1,89 @@
+/* Declare a variable called username.*/
+var userName;
+
+/* Declare a variable called myName & assign to it a string
+that represents your Full Name.*/
+
+var myName = "Rameez Malik";
+
+/*Write a script to
+a. declare a JS variable, titled message
+b. assign “Hello World” to the variable message
+c. display the message in alert box
+*/
+var message;
+message = "Hello World";
+//alert(message);
+
+/*
+Write a script to save student’s bio data in JS variables
+and show the data in alert boxes. Example:
+*/
+var stuName = "Rameez";
+var rollNUm = "12";
+var age = "28 years old";
+var education = "I have done bscs";
+
+//alert(stuName);
+//alert(rollNUm);
+//alert(age);
+//alert(education);
+
+/*
+Write a script to display the following alert using one JS
+variable:
+*/
+var one = "Pizza\nPizz\nPiz\nPi\nP";
+
+//alert(one);
+
+/*
+6. Fortune Teller: Why pay a fortuneteller when you can
+just program your fortune yourself?
+Store the following into variables: number of children,
+partner’s name, geographic location, and job title.
+*/
+
+var origion = "You Will be a full stack developer in usa "
+var numOfChildren = "You will have 2 chldren ";
+var partName = "And Marry with ABC ";
+
+
+//alert(origion + partName + numOfChildren);
+
+/*
+7. Declare a variable called email & assign to it a string that
+represents your Email Address (e.g. abc@xyz.com). Show
+the below mentioned message in an alert box. (Hint: use
+string concatenation)
+*/
+var a = "My email is : ";
+var email = "ABC@gmail.com";
+//alert(a + email);
+
+/* 8. Declare a variable called book & give it the value “A
+smarter way to learn JavaScript”. Display the following
+message in an alert box
+*/
+var learner = "I am tring to learn from the book";
+var book = " 'A smarter way to learn JavaScript' ";
+//alert(learner + book)
+
+/*
+9. Use the variables, declared in exercise 4 & show the details
+giving meaning to data. Like “My name is Ali Khan”, “I am
+17 years old”, “I study in SSUET”, “I take Mobile & Cloud
+Computing, Module A classes”
+*/
+var stuName = "i am Rameez";
+var age = ", i am 28 years old,";
+var rollNUm = "My Roll No is 12,";
+var education = "I have done bscs";
+
+//alert (stuName + age + rollNUm + education);
+
+/*
+10. Write a script to display this in browser through JS.
+*/
+
+document.body.innerHTML = '<h1> Yay! I can write HTML content through Javascript... </h1>';
