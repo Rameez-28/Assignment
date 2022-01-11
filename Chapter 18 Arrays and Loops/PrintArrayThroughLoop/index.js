a235409f553283227745dcb8cc81b255c6532895
Rameez-28 <rameezmalik93@gmail.com>
2022-01-11 19:54:46 +0500

    chapter 17 done

---
+++ Chapter 18 Arrays and Loops/PrintArrayThroughLoop/index.js
@@ -0,0 +1,28 @@
+
+document.title = ("Array Name Through Loop");
+/*
+5. Write a program to print items of the following array
+using for loop:
+fruits = [“apple”, “banana”, “mango”, “orange”,
+“strawberry”]
+*/
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+// for (var a = 0; a < 5; a++) {
+//     document.write(fruits[a] + "<br>");
+// }
+// document.write("<br>");
+// for (var x = 0; x < fruits.length; x++) {
+
+//     document.write("Element at index " + x + " is " + fruits[x] + "<br>");
+// }
+
+/*
+6. Generate the following series in your browser. See
+example output.
+a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+*/
