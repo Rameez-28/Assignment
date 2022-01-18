a08165ae2186be30f668cd7a483d1504502432d7
Rameez-28 <rameezmalik93@gmail.com>
2022-01-18 19:57:23 +0500

    assignment uploading

---
+++ chapter38-42/index.js
@@ -0,0 +1,48 @@
+// document.title = ("Chapter 38 to 42");
+
+
+/*
+While loop
+*/
+// var a = 1;
+// while (a <= 10) {
+//   document.write(`This is while loop<br>`);
+//   a++;
+// }
+
+/*
+// While loop
+*/
+// var a = 1;
+// do {
+//   document.write(`<br>This is do while loop`);
+//   a++;
+// }
+// while (a <= 10)
+
+/*
+1. Write a custom function power ( a, b ), to calculate the value of
+a raised to b.
+*/
+
+
+
+/*
+2. Any year is entered through the keyboard. Write a function to
+determine whether the year is a leap year or not.
+*/
+
+function leap(year) 
+{
+if (year%4 == 0 )
+{
+  document.write(`${year}  is Leap year `);
+}
+else
+{
+document.write(`${year}  is not Leap year `);
+}
+}
+
+var a = +prompt(`Please enter year`);
+leap(a);
